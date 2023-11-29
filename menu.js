9bf3e468ef9d130ccac514558c40a157d0f8090e
JeffreyLee <s10258143@connect.np.edu.sg>
2023-11-29 14:38:23 +0800

    Improved Responsive Menu Page, Improved Reponsive Checkout Page

--- menu.js
+++ menu.js
@@ -1,24 +1,24 @@
-window.addEventListener("scroll", function () {
-  const thresholdHotDeal = 1000;
-  const thresholdChicken = 1600;
-  const thresholdBoxes = 2352;
-  const thresholdBurger = 3199;
-  const thresholdWrap = 3699;
-  const thresholdDrink = 4914;
-  const thresholdSide = 6022;
-  const thresholdBowl = 6500;
-  const thresholdFor4 = 8000;
-  const currentScroll = window.scrollY || window.pageYOffset;
+// window.addEventListener("scroll", function () {
+//   const thresholdHotDeal = 1000;
+//   const thresholdChicken = 1600;
+//   const thresholdBoxes = 2352;
+//   const thresholdBurger = 3199;
+//   const thresholdWrap = 3699;
+//   const thresholdDrink = 4914;
+//   const thresholdSide = 6022;
+//   const thresholdBowl = 6500;
+//   const thresholdFor4 = 8000;
+//   const currentScroll = window.scrollY || window.pageYOffset;
 
-  if (currentScroll < thresholdHotDeal) {
-    selectCategory(currentScroll);
-  }
-});
+//   if (currentScroll < thresholdHotDeal) {
+//     selectCategory(currentScroll);
+//   }
+// });
 
-function selectCategory(currentScroll) {
-  // Replace this with the code you want to execute
-  console.log(currentScroll);
-}
+// function selectCategory(currentScroll) {
+//   // Replace this with the code you want to execute
+//   console.log(currentScroll);
+// }
 
 const navbar = document.querySelector("#nav-horizontal");
 
