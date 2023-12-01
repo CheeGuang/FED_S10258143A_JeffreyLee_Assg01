7ec5fbe580f9f24463de3cbc649ae4e2beafa780
JeffreyLee <s10258143@connect.np.edu.sg>
2023-12-01 16:50:50 +0800

    Contact us Animation

---
+++ contact-us.js
@@ -0,0 +1,10 @@
+// Apply Promo Animation
+function displaySubmitAnimation() {
+  document.getElementById("contact-us-submit-animation").style.display =
+    "block";
+  setTimeout(() => {
+    document.getElementById("contact-us-submit-animation").style.display =
+      "none";
+  }, "2150");
+  console.log("HIHH");
+}
